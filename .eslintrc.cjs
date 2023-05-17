0d17c256fee0c00542e2251507e08d3b05398dd8
Christopher Mickle <cd.mickle@yahoo.com>
2023-05-16 17:12:20 -0700

    Added jsx a11y to project for linting, fixed onSubmit type error.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,22 @@
 module.exports = {
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': 'warn',
+  env: {
+    browser: true,
+    "node":true,
+    es2021: true,
   },
-}
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:react/jsx-runtime",
+  ],
+  overrides: [],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint", "jsx-a11y"],
+  rules: {},
+};
--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,15 @@ import { FormEventHandler } from "react";
 import { useState, useEffect } from "react";
 import Todo from "./components/Todo";
 import { db } from "./components/Firebase";
-import { query, collection, onSnapshot, updateDoc, doc, addDoc, deleteDoc } from "firebase/firestore";
+import {
+  query,
+  collection,
+  onSnapshot,
+  updateDoc,
+  doc,
+  addDoc,
+  deleteDoc,
+} from "firebase/firestore";
 
 interface TodoType {
   text?: string;
@@ -57,14 +65,29 @@ const App = () => {
   return (
     <div className="h-screen w-screen p-4 bg-teal-300 overflow-y-scroll">
       <div className="bg-slate-100 max-w-[500px] w-full m-auto rounded-md shadow-xl p-4">
-        <h1 className="text-3xl font-bold text-center text-gray-800 p-2">A DEV'S TODO LIST</h1>
+        {/* <h1 className="text-3xl font-bold text-center text-gray-800 p-2">
+          A DEV'S TODO LIST
+        </h1> */}
+        {/* Don't use caps inside of text as it's bad for accessibility, instead write generally and then use css to capitalize */}
+        <h1 className="text-3xl font-bold text-center text-gray-800 p-2 uppercase">
+          A dev&apos;s to-do list
+        </h1>
         <Form createTodo={createTodo} input={input} setInput={setInput} />
         <ul>
           {todo.map((todos, index) => (
-            <Todo key={index} todos={todos} toggleComplete={toggleComplete} deleteTodo={deleteTodo} />
+            <Todo
+              key={index}
+              todos={todos}
+              toggleComplete={toggleComplete}
+              deleteTodo={deleteTodo}
+            />
           ))}
         </ul>
-        <p className="text-center">{`You have ${todo.length} task left!`}</p>
+        <p className="text-center">
+          {todo.length && todo.length === 1
+            ? "You have one task left"
+            : `You have ${todo.length} tasks left`}
+        </p>
       </div>
     </div>
   );
